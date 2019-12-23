ee4349cef534bdd8ec0783b575f2ce1321eccb1b
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2019-12-23 13:04:50 +0100

    Bump version for a first full deployment to v1.0.1.

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ rtd_url =     "https://" + projectName + ".readthedocs.io/en/latest/"
 
 setuptools.setup(
 	name=projectName,
-	version="1.0.0",
+	version="1.0.1",
 
 	author="Patrick Lehmann",
 	author_email="Paebbels@gmail.com",
